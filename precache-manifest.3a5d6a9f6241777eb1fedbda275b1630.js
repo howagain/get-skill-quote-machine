4deb5ec332e077f6531b98720118fe1cfaa33293
Jacob Haugen <howagain@gmail.com>
2020-08-03 14:08:09 -0400

    Updates

---
+++ precache-manifest.3a5d6a9f6241777eb1fedbda275b1630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadeca35554399343dbd20a6f02dd6c3",
+    "url": "/get-skill-quote-machine/index.html"
+  },
+  {
+    "revision": "1792afbba632262b57f2",
+    "url": "/get-skill-quote-machine/static/css/main.9ec7b5a0.chunk.css"
+  },
+  {
+    "revision": "14fe9bf3cf2a89faf2cd",
+    "url": "/get-skill-quote-machine/static/js/2.c8080839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/get-skill-quote-machine/static/js/2.c8080839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1792afbba632262b57f2",
+    "url": "/get-skill-quote-machine/static/js/main.22e95f47.chunk.js"
+  },
+  {
+    "revision": "d3158c79a771cdb231b9",
+    "url": "/get-skill-quote-machine/static/js/runtime-main.1aad8b1f.js"
+  }
+]);
